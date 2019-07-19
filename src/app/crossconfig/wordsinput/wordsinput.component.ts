05193f32cbbaf9e136bd31ad5f59164f387af220
Simarra <loic.martel@outlook.com>
2019-07-19 11:26:20 +0200

    MVP inputs OK

--- src/app/crossconfig/wordsinput/wordsinput.component.ts
+++ src/app/crossconfig/wordsinput/wordsinput.component.ts
@@ -7,15 +7,18 @@ import { FormGroup, FormBuilder, FormControl, FormArray, Validators } from '@ang
   styleUrls: ['./wordsinput.component.scss'],
 })
 export class WordsinputComponent implements OnInit {
-public form: FormGroup;
+  public form: FormGroup;
   public WordDecrList: FormArray;
+
+  public letter_count: number;
+  public longer_word_len: number;
 
   // returns all form groups under contacts
   get wordDescrFormGroup() {
     return this.form.get('words') as FormArray;
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -46,12 +49,24 @@ public form: FormGroup;
   }
 
 
-  // get the formgroup under contacts form array
-  getContactsFormGroup(index): FormGroup {
-    // this.contactList = this.form.get('contacts') as FormArray;
-    const formGroup = this.WordDecrList.controls[index] as FormGroup;
-    return formGroup;
+  updateLetterCount() {
+    // Update letter and word count.
+    let word_len: number = 0;
+    let word_len_count:number = 0;
+    for (let worddescr of this.form.value.words) {
+      let word:string = worddescr.word;
+      let tmp_word_len:number = word.length;
+      if (tmp_word_len > word_len){
+        word_len = tmp_word_len;
+      }
+      word_len_count += tmp_word_len;
+    };
+
+    this.letter_count = word_len_count;
+    this.longer_word_len = word_len;
+
   }
+  ;
 
   // method triggered when form is submitted
   submit() {
